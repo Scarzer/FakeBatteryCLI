b7b237ab29203e517e858e2c4476688610f38555
Irving Derin <Irving.Derin@gmail.com>
2016-05-16 01:25:58 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,130 @@
+#include <ctype.h>
+#include <cstring>
+#include <unistd.h>
+#include <malloc.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include "include/cmdline.h"
+
+
+#define NETLINK_USER 31
+
+#define MAX_PAYLOAD 1024 /* maximum payload size*/
+struct sockaddr_nl src_addr, dest_addr;
+struct nlmsghdr *nlh = NULL;
+struct iovec iov;
+int sock_fd;
+struct msghdr msg;
+
+
+int send_hello();
+int send_to_kern(std::string);
+
+int main(int argc, const char* argv[]) {
+
+    cmdline::parser a;
+
+    a.add<std::string>("command", 'c', "Use a command (Add|Remove|Modify|Enum|Stop)", true, "");
+    a.add<int>("battery", 'b', "Battery Number", false, 0);
+    a.add<int>("PARAMETERS", 'P', "Parameteres for batteries", false, 0);
+    a.add<std::string>("test_val", 't', "Test Value", false, "");
+
+    a.parse_check(argc, argv);
+
+    auto command = a.get<std::string>("command");
+    if(command == "add"){
+        std::cout << "Adding a battery" << std::endl;
+    } else if (command == "test"){
+        std::cout << "Testing my commands here" << std::endl;
+        send_to_kern("Gee thanks!");
+        send_to_kern("Some stuff");
+        send_to_kern("Blah Blah");
+        send_to_kern("WOOO");
+    } else std::cout << "Try Again" << std::endl;
+
+}
+
+// Sends string to the kernel!
+int send_to_kern(std::string thingToSend){
+    sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
+    if (sock_fd < 0)
+        return -1;
+
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.nl_family = AF_NETLINK;
+    src_addr.nl_pid = getpid(); /* self pid */
+
+    bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
+
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_pid = 0; /* For Linux Kernel */
+    dest_addr.nl_groups = 0; /* unicast */
+
+    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    nlh->nlmsg_pid = getpid();
+    nlh->nlmsg_flags = 0;
+
+    strcpy((char *)NLMSG_DATA(nlh), thingToSend.c_str());
+
+    iov.iov_base = (void *)nlh;
+    iov.iov_len = nlh->nlmsg_len;
+    msg.msg_name = (void *)&dest_addr;
+    msg.msg_namelen = sizeof(dest_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    printf("Sending message to kernel\n");
+    sendmsg(sock_fd, &msg, 0);
+    printf("Waiting for message from kernel\n");
+
+    /* Read message from kernel */
+    recvmsg(sock_fd, &msg, 0);
+    printf("Received message payload: %s\n", NLMSG_DATA(nlh));
+    close(sock_fd);
+}
+
+int send_hello(){
+    sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_USER);
+    if (sock_fd < 0)
+        return -1;
+
+    memset(&src_addr, 0, sizeof(src_addr));
+    src_addr.nl_family = AF_NETLINK;
+    src_addr.nl_pid = getpid(); /* self pid */
+
+    bind(sock_fd, (struct sockaddr *)&src_addr, sizeof(src_addr));
+
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    memset(&dest_addr, 0, sizeof(dest_addr));
+    dest_addr.nl_family = AF_NETLINK;
+    dest_addr.nl_pid = 0; /* For Linux Kernel */
+    dest_addr.nl_groups = 0; /* unicast */
+
+    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    nlh->nlmsg_pid = getpid();
+    nlh->nlmsg_flags = 0;
+
+    strcpy((char *)NLMSG_DATA( nlh), "Hello");
+
+    iov.iov_base = (void *)nlh;
+    iov.iov_len = nlh->nlmsg_len;
+    msg.msg_name = (void *)&dest_addr;
+    msg.msg_namelen = sizeof(dest_addr);
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+
+    printf("Sending message to kernel\n");
+    sendmsg(sock_fd, &msg, 0);
+    printf("Waiting for message from kernel\n");
+
+    /* Read message from kernel */
+    recvmsg(sock_fd, &msg, 0);
+    printf("Received message payload: %s\n", NLMSG_DATA(nlh));
+    close(sock_fd);
+}
